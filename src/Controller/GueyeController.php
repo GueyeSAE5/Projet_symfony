a503f6b424b5d64c5e641b0f9f975f943e2e06c8
GueyeSAE5 <baye.niass.gueye@etu.univ-st-etienne.fr>
2025-01-18 13:55:07 +0100

    mon controller

---
+++ src/Controller/GueyeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class GueyeController extends AbstractController
+{
+    #[Route('/gueye', name: 'app_gueye')]
+    public function index(): Response
+    {
+        return $this->render('gueye/index.html.twig', [
+            'controller_name' => 'GueyeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('/gueye/home.html.twig', [
+            'title' => "home",
+        ]);
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('/gueye/cv.html.twig', [
+            'title' => "cv",
+        ]);
+    }
+
+    #[Route('/loisir', name: 'app_loisir')]
+    public function loisir(): Response
+    {
+        return $this->render('/gueye/loisir.html.twig', [
+            'title' => "loisir",
+        ]);
+    }
+
+    #[Route('/Eportfolio', name: 'app_Eportfolio')]
+    public function eportfolio(): Response
+    {
+        return $this->render('/gueye/Eportfolio.html.twig', [
+            'title' => "Eportfolio",
+        ]);
+    }
+
+    #[Route('/formulaire', name: 'app_formulaire')]
+    public function formulaire(): Response
+    {
+        return $this->render('/gueye/formulaire.html.twig', [
+            'title' => "formulaire",
+        ]);
+    }
+}
